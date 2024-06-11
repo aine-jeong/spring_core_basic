35eaaf6750154576a555902d3bc0c10d2e20f964
ain <aine0105@naver.com>
2024-06-11 10:51:58 +0900

    section8. 빈 생명주기 콜백 > 빈 생명주기 콜백 시작

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,52 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+
+        /*
+        생성자 호출, url = null
+        connect: null
+        call: null / message = 초기화 연결 메시지
+
+        객체를 생성하는 단계에는 url이 없고, 객체를 생성한 다음에 외부에서 수정자 주입을 통해서 'setUrl()'이 호출되어야 url이 존재하게 된다.
+
+        "객체 생성 -> 의존관계 주입"
+
+        스프링은 의존관계 주입이 완료되면 스프링 빈에게 콜백 메서드를 통해 초기화 시점을 알려주는 다양한 기능을 제공한다.
+        또한 스프링은 스프링 컨테이너가 종료되기 직전에 소멸 콜백을 준다.
+
+        ### 스프링 빈의 이벤트 라이프사이클
+        "스프링 컨테이너 생성 -> 스프링 빈 생성 -> 의존관계 주입 -> 초기화 콜백 -> 사용 -> 소멸전 콜백 -> 스프링 종료"
+        - 초기화 콜백: 빈이 생성되고, 빈의 의존관계 주입이 완료된 후 호출
+        - 소멸전 콜백: 빈이 소멸되기 적전에 호출
+
+        - (tip) 객체의 생성과 초기화를 분리하자
+            : 생성자는 필수 정보(파라미터)를 받고, 메모리를 할당해서 객체를 생성하는 책임을 가진다.
+                반면 초기화는 이렇게 생성된 값들을 활용해서 외부 커넥션을 연결하는 등 무거운 동작을 수행한다.
+                따라서 생성자 안에서 무거운 초기화 작업을 함께 하는 것 보다는 객체를 생성하는 부분과 초기화하는 부분을 명확하게 나누는 것이 유지보수 관점에서 좋다.
+                물론 초기화 작업이 내부 값들만 약간 변경하는 정도로 단순한 경우에는 생성자에서 한번에 다 처리하는게 더 나을 수도 있다.
+         */
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
